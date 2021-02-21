e91752995b0a2a80d3f2a99e233559b189e84b68
ogarcia <ogarcia@portel.es>
2021-02-21 18:49:25 +0100

    añadir validación correo google

--- controllers/authController.js
+++ controllers/authController.js
@@ -93,9 +93,14 @@ const renewToken = async(req, res = response ) => {
 
     // Generar el TOKEN JWD
     const token = await generarJWT( uid );
+
+    // Obtener el usuario por UID
+    const usuario = await Usuario.findById( uid );
+
     res.json({
         ok: true,
-        token
+        token,
+        usuario
     })
 }
  
